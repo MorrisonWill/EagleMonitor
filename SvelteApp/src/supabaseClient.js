1df5c458cbf5946098877d67932b683cf9c9091f
Will Morrison <github@willrmorrison.com>
2021-12-22 20:11:23 -0500

    Removing old svelte code

--- SvelteApp/src/supabaseClient.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@supabase/supabase-js'
-
-const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYzNjU2ODc3NSwiZXhwIjoxOTUyMTQ0Nzc1fQ.CwUnJhI0_fMS5O77DxLKkhHf9ULziq5eCvHwEY9z5RI"
-const supabaseUrl = "https://npthxfsjlkgtythiccdg.supabase.co"
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey)
