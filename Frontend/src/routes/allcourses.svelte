f56077c9796ce9e9ff301818b0f1eb78ccf8243b
Aierus <clarkui@bc.edu>
2021-12-30 13:03:08 -0500

    style table

--- Frontend/src/routes/allcourses.svelte
+++ Frontend/src/routes/allcourses.svelte
@@ -60,9 +60,12 @@
   test();
 
   getCourses(from, to);
+
+  export let style;
+  let blueStyle="blueTable";
 </script>
 
-<table class="blueTable">
+<table class="blueTable"> 
   <thead>
     <tr>
       <th>ID</th>
@@ -94,58 +97,22 @@
   </tbody>
 </table>
 
-{#if from != 0}
-  <button on:click={previousPage}>previous</button>
-{/if}
-<button on:click={nextPage}>next</button>
 
 <style>
  table, th, td {
-  border: 1px solid;
+  border: 2px solid;
   border-collapse: collapse;
-  margin: 10px;
-  
+  margin: 10px; 
  }
  
- table.redTable {
-  border: 2px solid #A40808;
-  background-color: #EEE7DB;
-  width: 100%;
-  text-align: center;
-  border-collapse: collapse;
- }
- table.redTable td, table.redTable th {
-  border: 1px solid #AAAAAA;
-  padding: 3px 2px;
- }
- table.redTable tbody td {
-  font-size: 13px;
- }
- table.redTable tr:nth-child(even) {
-  background: #F5C8BF;
- }
- table.redTable thead {
-  background: #A40808;
-  background: -moz-linear-gradient(top, #bb4646 0%, #ad2020 66%, #A40808 100%);
-  background: -webkit-linear-gradient(top, #bb4646 0%, #ad2020 66%, #A40808 100%);
-  background: linear-gradient(to bottom, #bb4646 0%, #ad2020 66%, #A40808 100%);
- }
- table.redTable thead th {
-  font-size: 19px;
-  font-weight: bold;
-  color: #FFFFFF;
-  text-align: center;
-  border-left: 2px solid #A40808;
- }
- table.redTable thead th:first-child {
-  border-left: none;
- }table.blueTable {
+ table.blueTable {
   border: 1px solid #1C6EA4;
   background-color: #EEEEEE;
   width: 100%;
   text-align: left;
   border-collapse: collapse;
  }
+
  table.blueTable td, table.blueTable th {
   border: 1px solid #AAAAAA;
   padding: 3px 2px;
@@ -173,3 +140,9 @@
   border-left: none;
  } 
 </style>
+
+{#if from != 0}
+  <button on:click={previousPage}>previous</button>
+{/if}
+<button on:click={nextPage}>next</button>
+
