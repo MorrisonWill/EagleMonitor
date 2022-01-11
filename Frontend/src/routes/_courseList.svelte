d7f07016bd9c9aea713b7712661d4ab0f0dfbb9f
MorrisonWill <github@willrmorrison.com>
2022-01-10 19:06:19 -0500

    Changed ordering of courses

--- Frontend/src/routes/_courseList.svelte
+++ Frontend/src/routes/_courseList.svelte
@@ -11,7 +11,7 @@
     let { data: courses } = await supabase
       .from("courses")
       .select("*")
-      .order("id", { ascending: false })
+      .order("id", { ascending: true })
       .range(start, end);
     rows = courses;
   }
